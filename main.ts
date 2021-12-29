0533bdf422740f849c7a204e1c74176faa18e047
tobbyhans <everyfix@hotmail.com>
2021-12-29 13:22:25 +0100

    update sond

--- main.ts
+++ main.ts
@@ -1,12 +1,7 @@
 let Num = 9
 for (let Num = 0; Num <= 9; Num++) {
     basic.showNumber(Num)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    music.playMelody("D G F D F E D C ", 100)
+    music.playTone(988, music.beat(BeatFraction.Whole))
 }
+basic.showIcon(IconNames.House)
+music.playMelody("C5 B A G F E D C ", 120)

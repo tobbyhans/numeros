acedbcb101383983a44985cb4df826bae89352bf
tobbyhans <everyfix@hotmail.com>
2022-01-06 01:40:36 +0100

    ciclo infinito

--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
-let Num = 9
-for (let Num = 0; Num <= 9; Num++) {
-    basic.showNumber(Num)
-    music.playTone(988, music.beat(BeatFraction.Whole))
-}
-basic.showIcon(IconNames.House)
-music.playMelody("C5 B A G F E D C ", 120)
+let Num = 0
+basic.forever(function () {
+    Num = 9
+    for (let Num2 = 0; Num2 <= 9; Num2++) {
+        basic.showNumber(Num2)
+        music.playTone(262, music.beat(BeatFraction.Whole))
+    }
+    basic.showIcon(IconNames.House)
+    music.playMelody("C5 B A G F E D C ", 120)
+})

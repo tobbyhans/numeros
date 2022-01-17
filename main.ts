68d586855a1259d227b52a8da77d179b1ca125fc
tobbyhans <everyfix@hotmail.com>
2022-01-17 23:25:01 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,8 +3,5 @@ basic.forever(function () {
     Num = 9
     for (let Num2 = 0; Num2 <= 9; Num2++) {
         basic.showNumber(Num2)
-        music.playTone(262, music.beat(BeatFraction.Whole))
     }
-    basic.showIcon(IconNames.House)
-    music.playMelody("C5 B A G F E D C ", 120)
 })

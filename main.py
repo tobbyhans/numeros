a6f3e79ac474d06173f8b7687eda8a1e1f29aa05
tobbyhans <everyfix@hotmail.com>
2022-02-03 23:22:17 +0100

    for

---
+++ main.py
@@ -0,0 +1,3 @@
+Num = 9
+for Num2 in range(10):
+    basic.show_number(Num2)
--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-let Num = 9
-for (let Num = 0; Num <= 9; Num++) {
-    basic.showNumber(Num)
-    music.playTone(988, music.beat(BeatFraction.Whole))
+let Num = 0
+while (true) {
+    for (let Num = 0; Num <= 9; Num++) {
+        basic.showNumber(Num)
+    }
+    Num = 9
 }
-basic.showIcon(IconNames.House)
-music.playMelody("C5 B A G F E D C ", 120)
